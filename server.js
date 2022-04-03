ebbd8b3012ccb8d279ce861211c286e28d1cc511
Falcou <falcou@iMac-de-Falcou.local>
2022-04-03 17:13:09 +0200

    sorting dirs

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const port = 5000;                  //Save the port number where your server wil
 
 //Idiomatic expression in express to route and respond to a client request
 app.get('/', (req, res) => {        //get requests to the root ("/") will route here
-    res.sendFile('index.html', {root: __dirname});      //server responds by sending the index.html file to the client's browser
+    res.sendFile('./public/index.html', {root: __dirname});      //server responds by sending the index.html file to the client's browser
                                                         //the .sendFile method needs the absolute path to the file, see: https://expressjs.com/en/4x/api.html#res.sendFile 
 });
 
